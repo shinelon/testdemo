9a8b21825226b98234e824d36c951cffa6115297
shinelon <syq_sunshine@163.com>
2017-01-09 14:06:23 +0800

    add shutdownhookdemo

---
+++ src/main/java/demo/shutdownhook/ShutDownHook.java
@@ -0,0 +1,15 @@
+package demo.shutdownhook;
+
+public class ShutDownHook {
+	public static void main(String[] args) {
+		System.out.println("do something");
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+			System.out.println("do resource clean");
+		}));
+	}
+}
